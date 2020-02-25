dad554f17236edbab52139eff949f52c08265e11
Stefan De SChuyter <stefan.deschuyter@gmail.com>
2020-02-25 14:25:20 +0100

    First Webserver code testing

--- mysql.js
+++ mysql.js
@@ -4,34 +4,20 @@
 var myip = require('ip');
 var myos = require('os');
 var http = require('http');
+var RetRecords = [];
 const hostname = '0.0.0.0';
 // const port = 8088;
-//const hostname = '127.0.0.1';
 const port = 4000;
-//
-// MySQL hookup
-//
-const mysql = require('mysql');
-const SQLconn = mysql.createConnection({
-  host: '192.168.2.2',
-  user: 'user',
-  password: 'HPE!nvent',
-  database: 'sample'
-});
 //
 // Define Webserver hookup
 //
 const server = http.createServer((req, res) => {
-  /* 
   res.writeHead(200, {"Content-Type": "text/html"});
   res.write("<b>** Sample NodeJS Web Application **</b><br>");
-  res.write("<b>**   MySQL In Action -- 2020  **</b><br><br>");
+  res.write("<b>**    MySQL In Action -- 2020    **</b><br><br>");
   res.write("      Node Name: " + myos.hostname +  "<br>");
   res.write("   Node Address: " + myip.address() + " port: "+ server.address().port + "<br>");
-  res.write("    Time is now: " + Date().toString()+ "<br>");
-  /*
-  Define SQL Statement
-  */
+  res.write("    Time is now: " + Date().toString()+ "<br><br>");
   var urldata = req.url;   
   if (urldata == "/exit") { 
     res.write("*** webserver stopped ***<br>");
@@ -39,26 +25,34 @@ const server = http.createServer((req, res) => {
     res.end(); 
     process.exit();
   }
+  //
+  // MySQL hookup
+  //
+  const mysql = require('mysql');
+  const SQLconn = mysql.createConnection({
+    host: '192.168.2.2',
+    user: 'user',
+    password: 'HPE!nvent',
+    database: 'sample'
+  });
   SQLconn.connect((err) => {
     if (err) throw err;
     console.log('Connected!');
   });  
-  SQLconn.query('SELECT value, status, ts from DistanceData order by ts DESC LIMIT 50', function (err, rows) {
-    if (!err)  {
-        console.log('Data received from Db:');
-        // console.log(rows);
-        //  res.write("URL data: " + urldata);
-        //
-        // Loop SQL records received
-        //
-        res.write(rows.toString);
-        res.end();   
-    }
-    else {     
+  SQLconn.query('SELECT value, status, ts from DistanceData order by ts DESC LIMIT 10', function (err, rows) {
+      if (!err)  {
+        RetRecords= JSON.stringify(rows);
+      }
+      else {     
         res.write("*** No RECORDS RECEIVED ***<br>");
         console.log('Error while performing Query.');
       }
   });
+  res.write("*** JSON Query of last 10 records ***<br>");
+  res.write(RetRecords + "<br>");
+  console.log('=========');
+  console.log(RetRecords);
+  res.end();   
 });
 
 server.listen(port, hostname, () => {
